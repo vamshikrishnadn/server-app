020482ff8e69d7c7f40b77a34ba42c15d242a469
Vamhikrishnadn <vamshikrishnadn573@gmail.com>
2021-05-24 23:20:35 +0530

    hi updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
+  res.send({ hi: 'there' });
 });
 
 const PORT = process.env.PORT || 5000;
